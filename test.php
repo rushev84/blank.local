31e521f67917abc2be8060245dd6f214688bb62f
SergeyRushev <rushev84@yandex.ru>
2022-11-29 13:49:30 +0300

    create include areas

--- test.php
+++ test.php
@@ -1,7 +1,25 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?>
-
-test test
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "qqqq",
+		"EDIT_TEMPLATE" => ""
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "wwww",
+		"EDIT_TEMPLATE" => ""
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
