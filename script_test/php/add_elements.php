2a585901d484123aaa363b4cbc20ef413d8d3783
SergeyRushev <rushev84@yandex.ru>
2022-11-29 10:08:05 +0300

    fix: same random values

--- script_test/php/add_elements.php
+++ script_test/php/add_elements.php
@@ -4,24 +4,29 @@ $generator = new MyGenerator();
 
 $request = $_POST['request'];
 
-$words = ['rand_word1', 'rand_word2', 'rand_word3', 'rand_word4', 'rand_word5'];
+
+function getRandWord()
+{
+    $words = ['rand_word1', 'rand_word2', 'rand_word3', 'rand_word4', 'rand_word5'];
+    return $words[rand(0, count($words) - 1)];
+}
+
 
 for ($i = 1; $i <= $request['COUNT']; $i++) {
 
     $el = new CIBlockElement;
 
     $randNumber = rand();
-    $randWord = $words[rand(0, count($words) - 1)];
 
     $PROP = array();
 
     foreach ($request['properties'] as $property) {
-        $PROP[$property] = $randWord;
+        $PROP[$property] = getRandWord();
     }
 
     $arLoadProductArray = array(
         "IBLOCK_ID" => $request['IBLOCK_ID'],
-        "NAME" => $randWord,
+        "NAME" => getRandWord(),
         "CODE" => "code_{$randNumber}",
 
         "MODIFIED_BY" => $USER->GetID(), // элемент изменен текущим пользователем
@@ -30,18 +35,18 @@ for ($i = 1; $i <= $request['COUNT']; $i++) {
 
 
     if (in_array('PREVIEW_TEXT', $request['fields'])) {
-        $arLoadProductArray['PREVIEW_TEXT'] = $randWord;
+        $arLoadProductArray['PREVIEW_TEXT'] = getRandWord();
     }
 
     if (in_array('DETAIL_TEXT', $request['fields'])) {
-        $arLoadProductArray['DETAIL_TEXT'] = $randWord;
+        $arLoadProductArray['DETAIL_TEXT'] = getRandWord();
     }
 
     if (!empty($PROP)) {
         $arLoadProductArray['PROPERTY_VALUES'] = $PROP;
     }
 
-    in_array('ACTIVE', $request['fields']) ?  $arLoadProductArray['ACTIVE'] = 'Y' : $arLoadProductArray['ACTIVE'] = 'N';
+    in_array('ACTIVE', $request['fields']) ? $arLoadProductArray['ACTIVE'] = 'Y' : $arLoadProductArray['ACTIVE'] = 'N';
 
 //    dd($arLoadProductArray);
 
@@ -57,6 +62,6 @@ for ($i = 1; $i <= $request['COUNT']; $i++) {
 
 ?>
 
-<!--<span>--><?//= $request ?><!--</span>-->
+<!--<span>--><? //= $request ?><!--</span>-->
 
 
