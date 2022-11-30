b26f44bec7ae817464619d23e53a386367ea0bd4
SergeyRushev <rushev84@yandex.ru>
2022-11-30 12:03:47 +0300

    change version of program

--- iblocks_autocomplete/index.php
+++ iblocks_autocomplete/index.php
@@ -29,7 +29,7 @@ $iblocks = $generator->getIblocks();
 
 
 <div class="main">
-<p style="text-align: center; font-size: 16px; font-weight: bold">Автоматическое создание элементов инфоблока, v. 1.0</p>
+<p style="text-align: center; font-size: 16px; font-weight: bold">Автоматическое создание элементов инфоблока, v. 1.1 (30.11.2022)</p>
     <select class="js-select2" id="js-select2" name="city" style="width: 300px" onchange="showSettingsForm()">
         <option value=""></option>
         <?php foreach ($iblocks as $iblock) {
