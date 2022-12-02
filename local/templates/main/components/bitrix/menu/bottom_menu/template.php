7f959c363f273f3e018259141e41056d859e63a0
SergeyRushev <rushev84@yandex.ru>
2022-12-02 15:58:34 +0300

    add local components folder

---
+++ local/templates/main/components/bitrix/menu/bottom_menu/template.php
@@ -0,0 +1,28 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<?php if (!empty($arResult)): ?>
+
+   <div class="col-lg-7 col-md-7 col-sm-12">
+        <div class="footer-nav white-text">
+            <nav>
+                <ul>
+                    <?php foreach ($arResult as $item): ?>
+                        <?php if ($item['SELECTED']): ?>
+                            <li class="mega-parent">
+                                <a href="<?= $item['LINK'] ?>" style="color: #91711a"><?= $item['TEXT'] ?></a>
+                            </li>
+                        <?php else: ?>
+                            <li class="mega-parent">
+                                <a href="<?= $item['LINK'] ?>"><?= $item['TEXT'] ?></a>
+                            </li>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+<?php endif; ?>
+
+
+
