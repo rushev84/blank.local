bd47676800fa57992ca2488a35cd50dafa64c052
SergeyRushev <rushev84@yandex.ru>
2022-12-05 15:50:20 +0300

    add complex component bitrix:news

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,3 +1,11 @@
 <?php
 $arUrlRewrite=array (
+  0 => 
+  array (
+    'CONDITION' => '#^/test/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/test.php',
+    'SORT' => 100,
+  ),
 );
